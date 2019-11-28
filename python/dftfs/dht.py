8b66c54bc521a3b05a8cfae9f9e1baeb495b4b28
François Rozet <francois.rozet@outlook.com>
2019-11-28 22:01:19 +0100

    Simplification

--- python/dftfs/dht.py
+++ python/dftfs/dht.py
@@ -3,13 +3,12 @@ import requests
 
 class DHTNode(object):
     # Static variables
-    m = 256
-    size = 2 ** DHTNode.m
+    M = 2 ** 32
 
     # Static methods
     @staticmethod
     def hash(x):
-        return sha1(x)
+        return sha1(x) % DHTNode.M
 
     @staticmethod
     def address(host):
@@ -18,187 +17,108 @@ class DHTNode(object):
     @staticmethod
     def distance(a, b):
         if a > b:
-            return DHTNode.distance(b, a)
-        return min(b - a, DHTNode.size + a - b)
+            return M - distance(b, a)
+        return b - a
 
     @staticmethod
-    def between(x, a, b):
-        if a > b:
-            return not DHTNode.between(x, b, a)
-        return x > a and x < b
-
-    @staticmethod
-    def partition(dictionary, pivot):
-        below = list()
-        above = list()
-
-        for key, value in dictionary.items():
-            if key <= pivot:
-                below.append((key, value))
-            else:
-                above.append((key, value))
-
-        return dict(below), dict(above)
+    def between(a, b, c):
+        return distance(a, b) + distance(b, c) == distance(a, c)
 
     # Class methods
     def __init__(self, host):
         self.host = host
         self.id = hash(self.host)
 
-        self.predecessor = self.host
-        self.successor = self.host
+        self.predecessor = (self.id, self.host)
+        self.successor = (self.id, self.host)
 
-        self.host_table =  set(self.host)
+        self.host_table = {}
         self.hash_table = {}
 
     def join(self, host):
-        url = address(host) + '/successor?{:d}'.format(self.id)
-        self.update_successor(requests.get(url))
+        url = address(host) + '/lookup?{:d}'.format(self.id)
+        successor = requests.get(url)[0]
 
-        url = address(self.successor) + '/predecessor?{:d}'.format(self.id)
-        self.update_predecessor(requests.get(url))
+        url = address(successor) + '/predecessor'
+        predecessor = requests.get(url)
 
-        return # ok
+        self.update_predecessor(predecessor)
+        self.update_successor(successor)
 
     def update_successor(self, host):
-        if host == self.successor:
-            return # ok
+        if self.successor[1] != host:
+            old = self.successor
+            self.successor = (hash(host), host)
+            self.hash_table[self.successor[0]] = self.successor[1]
 
-        self.successor = host
-        self.host_table.add(self.successor)
-        below, above = DHTNode.partition(self.hash_table, self.id)
-
-        url = address(self.successor) + '/transfer'
-        resp = requests.post(url, table=above)
-        if resp != 200:
-            return # nok
-
-        self.hash_table, _ = DHTNode.partition(self.hash_table, DHTNode.hash(self.successor))
-
-        url = address(self.successor) + '/update_predecessor'
-        resp = requests.post(url, host=self.host)
-
-        return #ok
+            # Inform successor
+            url = address(self.successor[1]) + '/update_predecessor'
+            resp = requests.post(url, host=self.host)
 
     def update_predecessor(self, host):
-        if host == self.predecessor:
-            return # ok
+        if self.predecessor[1] != host:
+            old = self.predecessor
+            self.predecessor = (hash(host), host)
 
-        self.predecessor = host
-        self.host_table.add(self.predecessor)
-        below, above = DHTNode.partition(self.hash_table, self.id)
+            # Inform predecessor
+            url = address(self.predecessor[1]) + '/update_successor'
+            resp = requests.post(url, host=self.host)
 
-        url = address(self.predecessor) + '/transfer'
-        resp = requests.post(url, table=below)
-        if resp != 200:
-            return # nok
+            # Give responsability
+            if DHTNode.between(self.predecessor[0], old[0], self.id):
+                url = address(self.predecessor) + '/put'
+                for key, value in *hash_table: # del while iterating
+                    if DHTNode.between(key, old[0], self.predecessor[0]):
+                        resp = requests.post(url, key=value, value=value)
+                        del hash_table[key]
 
-        _, self.hash_table = DHTNode.partition(self.hash_table, DHTNode.hash(self.predecessor))
-
-        url = address(self.predecessor) + '/update_successor'
-        resp = requests.post(url, host=self.host)
-
-        return #ok
-
-    def predecessor(self, key):
-        host = self.lookup(key)[0]
-        url = address(host) + '/node_predecessor'
-        rest = requests.get(url)
-        return resp
-
-    def successor(self, key):
-        return self.lookup(key)[0]
-
-    def node_predecessor(self):
-        return self.predecessor
-
-    def node_successor(self):
-        return self.successor
+    def stabilize(self, chain):
+        for host in chain:
+            if host is not None:
+                self.hash_table[DHTNode.bash(host)] = host
 
     def lookup(self, key):
-        id_p = DHTNode.hash(self.predecessor)
-        id_s = DHTNode.hash(self.successor)
-
-        if key == id_p:
-            resp = requests.get(address(self.predecessor))
-            if resp.status_code != 200:
-                self.host_table.remove(self.predecessor)
-                # update_predecessor
-            else:
-                chain = [self.predecessor]
-        elif key == self.id or DHTNode.between(key, id_p, self.id):
+        if key == self.id or DHTNode.between(self.predecessor[0], key, self.id):
             chain = []
-        elif key == id_s or DHTNode.between(key, self.id, id_s):
-            resp = requests.get(address(self.successor))
-            if resp.status_code != 200:
-                self.host_table.remove(self.successor)
-                # update_successor
-            else:
-                chain = [self.successor]
         else:
             while True:
-                host = min(
+                id, host = min(
                     self.host_table,
-                    key=lambda x: DHTNode.distance(DHTNode.hash(x), key)
+                    key=lambda x: DHTNode.distance(x[0], key)
                 )
 
-                if host is self.host:
-                    return # nok
+                url = address(host)
+                resp = requests.get(url)
 
-                resp = requests.get(address(host))
                 if resp.status_code != 200:
-                    self.host_table.remove(host)
+                    if host == self.successor[1]:
+                        chain = [None]
+                        break
+
+                    del self.hash_table[id]
                 else:
+                    url = address(host) + '/lookup?{:d}'.format(key)
+                    chain = requests.get(url)                  
                     break
 
-            url = address(host) + '/lookup?{:d}'.format(key)
-            chain = requests.get(url)
-
-            self.host_table |= set(chain)
+        self.stabilize(chain)
 
         return chain + [self.host]
 
     def exists(self, key):
-        host = lookup(key)[0]
-
-        if host == self.host:
-            if key in self.hash_table:
-                return True
-            else:
-                return False
+        if key in self.hash_table:
+            return True
         else:
-            url = address(host) + '/exists?{:d}'.format(key)
-            rest = requests.get(url)
-            return resp
+            return False
 
     def get(self, key):
-        host = lookup(key)[0]
-
-        if host == self.host:
-            if key in self.hash_table:
-                return self.hash_table[key]
-            else:
-                return # nok
+        if self.exists(key):
+            return self.hash_table[key]
         else:
-            url = address(host) + '/get?{:d}'.format(key)
-            rest = requests.get(url)
-            return resp
+            return # nok
 
     def put(self, key, value):
-        host = lookup(key)[0]
-
-        if host == self.host:
-            if key in self.hash_table:
-                return # nok
-            else:
-                self.hash_table[key] = value
+        if self.exists(key):
+            return # nok
         else:
-            url = address(host) + '/put'
-            rest = requests.post(url, key=key, value=value)
-            return resp
-
-    def copy(self, src_key, dst_key)
-        value = self.get(src_key) # try catch
-        self.put(dst_key, value) # try catch
-
-        return # ok
+            self.hash_table[key] = value
